773f63c26dac6994ba68d2973b12a8fa374b382b
NiceGuy <bogdan.game.development@gmail.com>
2021-06-22 12:51:16 +0300

    Test

--- js/aboutPage.0ecd7425.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["aboutPage"],{"0bd2":function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("div",{staticClass:"page about"},[a("div",{staticClass:"content-space"},[a("h3",{attrs:{align:"center"}},[t._v("This is an about page")])])])}],c=a("2877"),i={},l=Object(c["a"])(i,n,s,!1,null,null,null);e["default"]=l.exports}}]);
-//# sourceMappingURL=aboutPage.0ecd7425.js.map
